ef0a91cdf7ce6d0eb051c46f7a9bea7cdbfb3dd6
Thato-Maboya <52561664+Thato-Maboya@users.noreply.github.com>
2019-11-02 11:46:07 +0200

    first commit

---
+++ redcap.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="index.css"/>
+<script type="text/javascript" src="index.js"></script>
+<body>
+
+<div class="w3-container">
+    <h1 class="w3-center">Introduction Workshop</h1>
+    <h2 class="w3-large">Two payment methods</h2>
+</div>
+
+<div class="w3-row ">
+    <div class="w3-container w3-half">
+        <div class="w3-card w3-container" style="min-height:440px">
+            <div class="w3-container"><h2 class="w3-center">R750</h2></div>
+                <div class="slider">
+                    <h1 id="boldTitle" style="color: tomato">REDCap</h1>
+                    <hr><br/><br/>
+                    <ul>
+                        <li><b>Time:</b> 08:30 - 15:00</li>
+                        <li><b>Cost:</b>
+                            <ul>
+                                <li>R750 Wits Faculty of Health Sciences staff or student</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+    <div class="w3-container w3-half">
+        <div class="w3-card w3-container" style="min-height:440px">
+            <div class="w3-container"><h2 class="w3-center">R2250</h2></div>
+                <div class="slider">
+                    <h1 id="boldTitle" style="color: tomato">REDCap</h1>
+                    <hr><br/><br/>
+                    <ul>
+                        <li><b>Time:</b> 08:30 - 15:00</li>
+                        <li><b>Cost:</b>
+                            <ul>
+                                <li>R2250 Non-Faculty members.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
